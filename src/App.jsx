0a554876fd7877dcf707444b6504317b3c306104
mukhtar4532 <mukhtarhussain4532@gmail.com>
2025-07-24 22:33:48 +0530

    implement some portion of hero section code and add gsap animation in nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <main>
+        <Navbar />
+        <Hero />
+      </main>
+    </>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,33 @@
+import { useGSAP } from "@gsap/react";
 import { navLinks } from "../../constants";
+import gsap from "gsap";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "tranparent" },
+      {
+        backgroundColor: "#00000050",
+        backgrounFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
   return (
     <>
       <nav>
         <div>
-          <a href="#home" className="flex items-center gap-2">
+          <a href="#home" className="flex items-center gap-2 ">
             <img src="/images/logo.png" alt="logo" />
             <p>Velvet Pour</p>
           </a>
